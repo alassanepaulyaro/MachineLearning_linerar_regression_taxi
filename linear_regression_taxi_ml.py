2f0050bb9d8d33a4c54b8c9b2d6964672906818d
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-11-05 11:52:34 +0100

    Validate Model

--- linear_regression_taxi_ml.py
+++ linear_regression_taxi_ml.py
@@ -378,3 +378,54 @@ matches the ground truth fare calculation. You should find that the model is
 roughly close to this formula.
 '''
 print(answer)
+
+# Validate Model
+
+#@title Code - Define functions to make predictions
+def format_currency(x):
+  return "${:.2f}".format(x)
+
+def build_batch(df, batch_size):
+  batch = df.sample(n=batch_size).copy()
+  batch.set_index(np.arange(batch_size), inplace=True)
+  return batch
+
+def predict_fare(model, df, features, label, batch_size=50):
+  batch = build_batch(df, batch_size)
+  predicted_values = model.predict_on_batch(x=batch.loc[:, features].values)
+
+  data = {"PREDICTED_FARE": [], "OBSERVED_FARE": [], "L1_LOSS": [],
+          features[0]: [], features[1]: []}
+  for i in range(batch_size):
+    predicted = predicted_values[i][0]
+    observed = batch.at[i, label]
+    data["PREDICTED_FARE"].append(format_currency(predicted))
+    data["OBSERVED_FARE"].append(format_currency(observed))
+    data["L1_LOSS"].append(format_currency(abs(observed - predicted)))
+    data[features[0]].append(batch.at[i, features[0]])
+    data[features[1]].append("{:.2f}".format(batch.at[i, features[1]]))
+
+  output_df = pd.DataFrame(data)
+  return output_df
+
+def show_predictions(output):
+  header = "-" * 80
+  banner = header + "\n" + "|" + "PREDICTIONS".center(78) + "|" + "\n" + header
+  print(banner)
+  print(output)
+  return
+
+#@title Code - Make predictions
+
+output = predict_fare(model_2, training_df, features, label)
+show_predictions(output)
+
+# How close is the predicted value to the label value?
+# -----------------------------------------------------------------------------
+answer = '''
+Based on a random sampling of examples, the model seems to do pretty well
+predicting the fare for a taxi ride. Most of the predicted values do not vary
+significantly from the observed value. You should be able to see this by looking
+at the column L1_LOSS = |observed - predicted|.
+'''
+print(answer)
