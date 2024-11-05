d34688449deff19623ede823b857ec3d585284f1
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-11-05 11:46:32 +0100

    Train a model

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# sandbox
+# Machine learning 
+## Linear Regression Taxi 
+### Train a model with one feature
+Training a Model to predict the cost of the fare using a single feature. This exemple use TRIP_MILES as the feature for training run. 
+<img src="Screenshot-Experiment-1.png" alt="Alt Text" width="1024" height="512">
+
+### Experiment with hyperparameters
+Multiple experiments to find the best set of hyperparameters to train a model.
+<img src="Screenshot-Experiment-2.png" alt="Alt Text" width="1024" height="512">
+
+#### Train a model with two features
+Training the model with two features, TRIP_MILES and TRIP_MINUTES
+<img src="Screenshot-Experiment-3.png" alt="Alt Text" width="1024" height="512">
--- linear_regression_taxi_ml.py
+++ linear_regression_taxi_ml.py
@@ -8,9 +8,6 @@ import io
 import numpy as np
 import pandas as pd
 
-import tensorflow as tf
-print(tf.__version__)
-
 # machine learning
 import keras
 
@@ -102,3 +99,282 @@ print(answer)
 sns.pairplot(training_df, x_vars=["FARE", "TRIP_MILES", "TRIP_SECONDS"], y_vars=["FARE", "TRIP_MILES", "TRIP_SECONDS"])
 
 ## Train Model
+
+#@title Define plotting functions
+
+def make_plots(df, feature_names, label_name, model_output, sample_size=200):
+
+  random_sample = df.sample(n=sample_size).copy()
+  random_sample.reset_index()
+  weights, bias, epochs, rmse = model_output
+
+  is_2d_plot = len(feature_names) == 1
+  model_plot_type = "scatter" if is_2d_plot else "surface"
+  fig = make_subplots(rows=1, cols=2,
+                      subplot_titles=("Loss Curve", "Model Plot"),
+                      specs=[[{"type": "scatter"}, {"type": model_plot_type}]])
+
+  plot_data(random_sample, feature_names, label_name, fig)
+  plot_model(random_sample, feature_names, weights, bias, fig)
+  plot_loss_curve(epochs, rmse, fig)
+
+  fig.show()
+  return
+
+def plot_loss_curve(epochs, rmse, fig):
+  curve = px.line(x=epochs, y=rmse)
+  curve.update_traces(line_color='#ff0000', line_width=3)
+
+  fig.append_trace(curve.data[0], row=1, col=1)
+  fig.update_xaxes(title_text="Epoch", row=1, col=1)
+  fig.update_yaxes(title_text="Root Mean Squared Error", row=1, col=1, range=[rmse.min()*0.8, rmse.max()])
+
+  return
+
+def plot_data(df, features, label, fig):
+  if len(features) == 1:
+    scatter = px.scatter(df, x=features[0], y=label)
+  else:
+    scatter = px.scatter_3d(df, x=features[0], y=features[1], z=label)
+
+  fig.append_trace(scatter.data[0], row=1, col=2)
+  if len(features) == 1:
+    fig.update_xaxes(title_text=features[0], row=1, col=2)
+    fig.update_yaxes(title_text=label, row=1, col=2)
+  else:
+    fig.update_layout(scene1=dict(xaxis_title=features[0], yaxis_title=features[1], zaxis_title=label))
+
+  return
+
+def plot_model(df, features, weights, bias, fig):
+  df['FARE_PREDICTED'] = bias[0]
+
+  for index, feature in enumerate(features):
+    df['FARE_PREDICTED'] = df['FARE_PREDICTED'] + weights[index][0] * df[feature]
+
+  if len(features) == 1:
+    model = px.line(df, x=features[0], y='FARE_PREDICTED')
+    model.update_traces(line_color='#ff0000', line_width=3)
+  else:
+    z_name, y_name = "FARE_PREDICTED", features[1]
+    z = [df[z_name].min(), (df[z_name].max() - df[z_name].min()) / 2, df[z_name].max()]
+    y = [df[y_name].min(), (df[y_name].max() - df[y_name].min()) / 2, df[y_name].max()]
+    x = []
+    for i in range(len(y)):
+      x.append((z[i] - weights[1][0] * y[i] - bias[0]) / weights[0][0])
+
+    plane=pd.DataFrame({'x':x, 'y':y, 'z':[z] * 3})
+
+    light_yellow = [[0, '#89CFF0'], [1, '#FFDB58']]
+    model = go.Figure(data=go.Surface(x=plane['x'], y=plane['y'], z=plane['z'],
+                                      colorscale=light_yellow))
+
+  fig.add_trace(model.data[0], row=1, col=2)
+
+  return
+
+def model_info(feature_names, label_name, model_output):
+  weights = model_output[0]
+  bias = model_output[1]
+
+  nl = "\n"
+  header = "-" * 80
+  banner = header + nl + "|" + "MODEL INFO".center(78) + "|" + nl + header
+
+  info = ""
+  equation = label_name + " = "
+
+  for index, feature in enumerate(feature_names):
+    info = info + "Weight for feature[{}]: {:.3f}\n".format(feature, weights[index][0])
+    equation = equation + "{:.3f} * {} + ".format(weights[index][0], feature)
+
+  info = info + "Bias: {:.3f}\n".format(bias[0])
+  equation = equation + "{:.3f}\n".format(bias[0])
+
+  return banner + nl + info + nl + equation
+
+print("SUCCESS: defining plotting functions complete.")
+
+#@title Code - Define ML functions
+
+def build_model(my_learning_rate, num_features):
+  """Create and compile a simple linear regression model."""
+  # Most simple keras models are sequential.
+  model = keras.models.Sequential()
+
+  # Describe the topography of the model.
+  # The topography of a simple linear regression model
+  # is a single node in a single layer.
+  model.add(keras.layers.Dense(units=1,
+                                  input_shape=(num_features,)))
+
+  # Compile the model topography into code that Keras can efficiently
+  # execute. Configure training to minimize the model's mean squared error.
+  model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=my_learning_rate),
+                loss="mean_squared_error",
+                metrics=[keras.metrics.RootMeanSquaredError()])
+
+  return model
+
+
+def train_model(model, df, features, label, epochs, batch_size):
+  """Train the model by feeding it data."""
+
+  # Feed the model the feature and the label.
+  # The model will train for the specified number of epochs.
+  # input_x = df.iloc[:,1:3].values
+  # df[feature]
+  history = model.fit(x=features,
+                      y=label,
+                      batch_size=batch_size,
+                      epochs=epochs)
+
+  # Gather the trained model's weight and bias.
+  trained_weight = model.get_weights()[0]
+  trained_bias = model.get_weights()[1]
+
+  # The list of epochs is stored separately from the rest of history.
+  epochs = history.epoch
+
+  # Isolate the error for each epoch.
+  hist = pd.DataFrame(history.history)
+
+  # To track the progression of training, we're going to take a snapshot
+  # of the model's root mean squared error at each epoch.
+  rmse = hist["root_mean_squared_error"]
+
+  return trained_weight, trained_bias, epochs, rmse
+
+
+def run_experiment(df, feature_names, label_name, learning_rate, epochs, batch_size):
+
+  print('INFO: starting training experiment with features={} and label={}\n'.format(feature_names, label_name))
+
+  num_features = len(feature_names)
+
+  features = df.loc[:, feature_names].values
+  label = df[label_name].values
+
+  model = build_model(learning_rate, num_features)
+  model_output = train_model(model, df, features, label, epochs, batch_size)
+
+  print('\nSUCCESS: training experiment complete\n')
+  print('{}'.format(model_info(feature_names, label_name, model_output)))
+  make_plots(df, feature_names, label_name, model_output)
+
+  return model
+
+print("SUCCESS: defining linear regression functions complete.")
+
+# Train a model with one feature
+
+#@title Code - Experiment 1
+
+# The following variables are the hyperparameters.
+learning_rate = 0.001
+epochs = 20
+batch_size = 50
+
+# Specify the feature and the label.
+features = ['TRIP_MILES']
+label = 'FARE'
+
+model_1 = run_experiment(training_df, features, label, learning_rate, epochs, batch_size)
+
+# How many epochs did it take to converge on the final model?
+# -----------------------------------------------------------------------------
+answer = """
+Use the loss curve to see where the loss begins to level off during training.
+
+With this set of hyperparameters:
+
+  learning_rate = 0.001
+  epochs = 20
+  batch_size = 50
+
+it takes about 5 epochs for the training run to converge to the final model.
+"""
+print(answer)
+
+# How well does the model fit the sample data?
+# -----------------------------------------------------------------------------
+answer = '''
+It appears from the model plot that the model fits the sample data fairly well.
+'''
+print(answer)
+
+# Experiment with hyperparameters
+
+#@title Code - Experiment 2
+
+# The following variables are the hyperparameters.
+learning_rate = 0.001
+epochs = 20
+batch_size = 50
+
+# Specify the feature and the label.
+features = ['TRIP_MILES']
+label = 'FARE'
+
+model_1 = run_experiment(training_df, features, label, learning_rate, epochs, batch_size)
+
+# Train a model with two features
+
+#@title Code - Experiment 3
+
+# The following variables are the hyperparameters.
+learning_rate = 0.001
+epochs = 20
+batch_size = 50
+
+training_df['TRIP_MINUTES'] = training_df['TRIP_SECONDS']/60
+
+features = ['TRIP_MILES', 'TRIP_MINUTES']
+label = 'FARE'
+
+model_2 = run_experiment(training_df, features, label, learning_rate, epochs, batch_size)
+
+# Does the model with two features produce better results than one using a
+# single feature?
+# -----------------------------------------------------------------------------
+answer = '''
+To answer this question for your specific training runs, compare the RMSE for
+each model. For example, if the RMSE for the model trained with one feature was
+3.7457 and the RMSE for the model with two features is 3.4787, that means that
+on average the model with two features makes predictions that are about $0.27
+closer to the observed fare.
+
+'''
+print(answer)
+
+# Does it make a difference if you use TRIP_SECONDS instead of TRIP_MILES?
+# -----------------------------------------------------------------------------
+answer = '''
+When training a model with more than one feature, it is important that all
+numeric values are roughly on the same scale. In this case, TRIP_SECONDS and
+TRIP_MILES do not meet this criteria. The mean value for TRIP_MILES is 8.3 and
+the mean for TRIP_SECONDS is 1320; that is two orders of magnitude difference.
+Converting the trip duration to minutes helps during training because in puts
+values for both features on a more comparable scale. Of course, this is not the
+only way to scale values before training, but you will learn about that in
+another module.
+
+'''
+print(answer)
+
+# How well do you think the model comes to the ground truth fare calculation for
+# Chicago taxi trips?
+# -----------------------------------------------------------------------------
+answer = '''
+In reality, Chicago taxi cabs use a documented formula to determine cab fares.
+For a single passenger paying cash, the fare is calculated like this:
+
+FARE = 2.25 * TRIP_MILES + 0.12 * TRIP_MINUTES + 3.25
+
+Typically with machine learning problems you would not know the 'correct'
+formula, but in this case you can this knowledge to evaluate your model. Take a
+look at your model output (the weights and bias) and determine how well it
+matches the ground truth fare calculation. You should find that the model is
+roughly close to this formula.
+'''
+print(answer)
