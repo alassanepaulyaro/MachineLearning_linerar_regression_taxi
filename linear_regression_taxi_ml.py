01a00f14db2fae0855c6753958c22fb99e0fcd76
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-11-05 10:02:49 +0100

    Dataset Exploration

--- linear_regression_taxi_ml.py
+++ linear_regression_taxi_ml.py
@@ -1,3 +1,4 @@
+## Setup
 
 #@title Code - Load dependencies
 #general
@@ -19,12 +20,12 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import seaborn as sns
 
-#Load the dataset
+# Load the dataset
 # @title
 chicago_taxi_dataset = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/chicago_taxi_train.csv")
 
 
-#Update the dataframe
+# Update the dataframe
 #@title Code - Read dataset
 # Updates dataframe to use specific columns.
 training_df = chicago_taxi_dataset[['TRIP_MILES', 'TRIP_SECONDS', 'FARE', 'COMPANY', 'PAYMENT_TYPE', 'TIP_RATE']]
@@ -33,4 +34,71 @@ print('Read dataset completed successfully.')
 print('Total number of rows: {0}\n\n'.format(len(training_df.index)))
 training_df.head(200)
 
-#Dataset Exploration
+## Dataset Exploration
+#@title Code - View dataset statistics
+
+print('Total number of rows: {0}\n\n'.format(len(training_df.index)))
+training_df.describe(include='all')
+
+#@title Double-click or run to view answers about dataset statistics
+
+answer = '''
+What is the maximum fare? 				              Answer: $159.25
+What is the mean distance across all trips? 		Answer: 8.2895 miles
+How many cab companies are in the dataset? 		  Answer: 31
+What is the most frequent payment type? 		    Answer: Credit Card
+Are any features missing data? 				          Answer: No
+'''
+
+# You should be able to find the answers to the questions about the dataset
+# by inspecting the table output after running the DataFrame describe method.
+#
+# Run this code cell to verify your answers.
+
+# What is the maximum fare?
+max_fare = training_df['FARE'].max()
+print("What is the maximum fare? \t\t\t\tAnswer: ${fare:.2f}".format(fare = max_fare))
+
+# What is the mean distance across all trips?
+mean_distance = training_df['TRIP_MILES'].mean()
+print("What is the mean distance across all trips? \t\tAnswer: {mean:.4f} miles".format(mean = mean_distance))
+
+# How many cab companies are in the dataset?
+num_unique_companies =  training_df['COMPANY'].nunique()
+print("How many cab companies are in the dataset? \t\tAnswer: {number}".format(number = num_unique_companies))
+
+# What is the most frequent payment type?
+most_freq_payment_type = training_df['PAYMENT_TYPE'].value_counts().idxmax()
+print("What is the most frequent payment type? \t\tAnswer: {type}".format(type = most_freq_payment_type))
+
+# Are any features missing data?
+missing_values = training_df.isnull().sum().sum()
+print("Are any features missing data? \t\t\t\tAnswer:", "No" if missing_values == 0 else "Yes")
+
+# Generate a correlation matrix
+
+
+#@title Code - View correlation matrix
+training_df.corr(numeric_only = True)
+# Which feature correlates most strongly to the label FARE?
+# ---------------------------------------------------------
+answer = '''
+The feature with the strongest correlation to the FARE is TRIP_MILES.
+As you might expect, TRIP_MILES looks like a good feature to start with to train
+the model. Also, notice that the feature TRIP_SECONDS has a strong correlation
+with fare too.
+'''
+print(answer)
+
+
+# Which feature correlates least strongly to the label FARE?
+# -----------------------------------------------------------
+answer = '''The feature with the weakest correlation to the FARE is TIP_RATE.'''
+print(answer)
+
+# Visualize relationships in dataset
+
+# @title Code - View pairplot
+sns.pairplot(training_df, x_vars=["FARE", "TRIP_MILES", "TRIP_SECONDS"], y_vars=["FARE", "TRIP_MILES", "TRIP_SECONDS"])
+
+## Train Model
